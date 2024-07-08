0621278c65df677f240dcf047622558744094324
xiaofeng <ann.simple.xf@gmail.com>
2024-07-08 09:57:00 +0800

    ofd 校验

--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module esl
 
 go 1.22.4
 
-require github.com/tjfoc/gmsm v1.4.1
-
 require (
-	golang.org/x/crypto v0.0.0-20201012173705-84dcc777aaee // indirect
-	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
+	github.com/itlabers/crypto v1.0.2-0.20210519065553-86f4e4883143
+	github.com/itlabers/ofd-go v0.0.0-20240312022202-ab26ade71f05
 )
+
+require golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"encoding/asn1"
 	"fmt"
-	"github.com/tjfoc/gmsm/x509"
+	"github.com/itlabers/crypto/x509"
+	"github.com/itlabers/ofd-go"
 	"os"
 )
 
@@ -30,7 +31,7 @@ func main() {
 		return
 	}
 	// 输出解码后的 Header
-	fmt.Printf(" x509.ParseCertificates certSign: %+v\n", certSign)
+	fmt.Printf(" x509.ParseCertificates certSign: %+v\n", certSign.Issuer.String())
 
 	certByte2 := sESeal.Cert
 	certMake, err := x509.ParseCertificates(certByte2)
@@ -38,14 +39,71 @@ func main() {
 		fmt.Println("Error cert ASN.1 data:", err)
 		return
 	}
-	fmt.Printf(" x509.ParseCertificates certMake: %+v\n", certMake)
+	fmt.Printf(" x509.ParseCertificates certMake: %+v\n", certMake[0].Issuer.String())
 
-	//CheckIntegrity(sESeal)
+	verify, err := OfdCheck()
+	if err != nil {
+		fmt.Println("Error cert ASN.1 data:", err)
+		return
+	}
+	fmt.Println("verify:", verify)
 
-	//if err := VerifySignature(sESeal); err != nil {
-	//	fmt.Println("Signature verification failed:", err)
-	//} else {
-	//	fmt.Println("Signature verification succeeded")
-	//}
+}
 
+func OfdCheck() (verify bool, err error) {
+	//path := "many-ofd.ofd"
+	//path := "./test-1.ofd"
+	path := "./output.ofd"
+	//path := "./test.ofd"
+	ofdReader, err := ofd.NewOFDReader(path, ofd.WithValidator(&ofd.CommonValidator{}))
+	if err != nil {
+		return verify, err
+	}
+	defer ofdReader.Close()
+	a, err := ofdReader.OFD()
+	if err != nil {
+		return verify, err
+	}
+
+	docIDList := []string{}
+	for _, v := range a.DocBody {
+		docIDList = append(docIDList, v.DocInfo.DocID.Text)
+	}
+	if len(docIDList) < 1 {
+		err = fmt.Errorf(" file don't have eseal")
+		return verify, err
+	}
+	flag := false
+	for _, docID := range docIDList {
+		signs, err := ofdReader.GetSignaturesById(docID)
+		if err != nil {
+			return verify, err
+		}
+		temFlag := false
+		for _, v := range signs.Signature {
+			sign, err := signs.GetSignatureById(v.ID)
+			if err != nil {
+				return verify, err
+			}
+			verify, err = sign.Verify()
+			if err != nil {
+				return verify, err
+			}
+			if !verify {
+				flag = true
+				temFlag = true
+				break
+			}
+		}
+		if temFlag {
+			break
+		}
+
+	}
+	if flag {
+		verify = false
+	} else {
+		verify = true
+	}
+	return verify, nil
 }
